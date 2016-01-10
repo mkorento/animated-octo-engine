ea5bd94e48adcfcb039b224e1a74ae1765ddb572
Mika Korento <korento.mika@gmail.com>
2016-01-11 00:42:19 +0200

    Add new image migration

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :edit, :update, :destroy]
 
--- db/migrate/20160110222810_create_images.rb
+++ db/migrate/20160110222810_create_images.rb
@@ -1,6 +1,9 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
+      t.string :name, null: false
+      t.string :description
+      t.string :file
 
       t.timestamps
     end
