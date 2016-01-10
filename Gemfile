415ae78d20ea6363937a68c25fc9d832ea672f79
Mika Korento <korento.mika@gmail.com>
2016-01-11 00:46:01 +0200

    Add image upload-feature

--- Gemfile
+++ Gemfile
@@ -38,3 +38,6 @@ gem 'spring',        group: :development
 # Use debugger
 # gem 'debugger', group: [:development, :test]
 
+gem "mini_magick"
+gem "carrierwave"
+gem "colorize"
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -26,16 +26,28 @@ class ImagesController < ApplicationController
   # POST /images
   # POST /images.json
   def create
-    @image = Image.new(image_params)
 
-    respond_to do |format|
+    if !image_params[:file].nil?
+      file_type = ".#{image_params[:file].content_type.split('/')[1]}"
+      file_name = "#{SecureRandom.urlsafe_base64}#{file_type}"
+
+      image_params[:file].original_filename = file_name
+      @image = Image.new(name: image_params[:name],
+                         description: image_params[:description],
+                         file: image_params[:file])
+
       if @image.save
-        format.html { redirect_to @image, notice: 'Image was successfully created.' }
-        format.json { render :show, status: :created, location: @image }
+        File.open(Rails.root.join('public/images', file_name), 'wb') do |file|
+          file.write(image_params[:file].read)
+        end
+
+        flash[:notice] = "Image was successfully uploaded."
+        redirect_to @image
       else
-        format.html { render :new }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
+        render :new
       end
+    else
+      redirect_to new_image_path
     end
   end
 
@@ -71,6 +83,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params[:image]
+      params.require(:image).permit(:name, :description, :file)
     end
 end
