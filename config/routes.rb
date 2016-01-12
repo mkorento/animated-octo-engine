4e3f8bded8513d4e2aebce0a80dd59e27ddb11ad
Mika Korento <korento.mika@gmail.com>
2016-01-12 12:20:50 +0200

    Change image urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'images#index'
-  resources :images
+  resources :images, path: :gallery
   post 'comments', to: 'comments#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
